d9d620e8ae229a2bfd53144f6ff71f2d63c93070
PeppermintSnow <peppermintsnow06@gmail.com>
2024-07-02 23:25:53 +0800

    Responsive web design layout for mobile completed; TODO: Responsive navbar using js;

---
+++ collapseNav.js
@@ -0,0 +1,14 @@
+/* const collapseBtn = document.getElementById("collapse-nav");
+const mainDoc = document.getElementById("main-doc");
+const navBar = document.getElementById("navbar")
+let e;
+
+collapseBtn.addEventListener("click", alert("Hello"));
+
+const openNav = () => {
+    navBar.style.width = "200px";
+}
+
+const closeNav = () => {
+    navBar.style.width = "0";
+} */
--- loadSrcCode.js
+++
@@ -1,6 +0,0 @@
-//const checkpoints = document.getElementById("checkpoints-code");
-
-let fileReader = new FileReader();
-fileReader.onload = () => {
-    console.log(fileReader.result);
-}
